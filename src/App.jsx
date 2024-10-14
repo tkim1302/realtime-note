ccdae718ca9a6aaa2a325e593fa1a2a39d312098
김태영 <gimtaeyeong@gimtaeyeong-ui-MacBookPro.local>
2024-10-15 03:47:57 +0900

    Fix: fix case home to Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Note from "./pages/Note";
 
 function App() {
