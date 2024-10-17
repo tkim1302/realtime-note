d0c240c1f9f7ad7ef9f35f8b82c7f72a8893426e
김태영 <zhtyzh0213@gmail.com>
2024-10-18 04:34:28 +0900

    Fix: fix image bug

--- src/components/SignInWithGoogle.jsx
+++ src/components/SignInWithGoogle.jsx
@@ -1,7 +1,7 @@
 function SignInWithGoogle({ handleLogin }) {
     return (
         <div className="flex w-80 h-28 rounded-xl bg-white gap-5">
-            <img src="/src/images/Google_logo.webp" alt="google logo" className="mt-6 ml-6 w-16 h-16"/>
+            <img src="/images/Google_logo.webp" alt="google logo" className="mt-6 ml-6 w-16 h-16"/>
             <button className="text-black" onClick={handleLogin}>Sign In With Google</button>
         </div>
     )
