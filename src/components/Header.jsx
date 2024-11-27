1d0ec05ecf2bb1bc1ce8571e9b8504434c7c3090
김태영 <zhtyzh0213@gmail.com>
2024-11-27 21:45:44 +0900

    Feat: navigate to / directory on logo click

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import useStore from "../utils/store";
 import ProfileImg from "./ProfileImg";
 
 const Header = () => {
   const { user } = useStore();
+  const navigate = useNavigate();
+
   return (
     <div className="flex justify-between mt-8">
-      <h1 className="text-white text-4xl ml-14 font-bold">RealtimeNote</h1>
+      <h1
+        className="text-white text-4xl ml-14 font-bold cursor-pointer"
+        onClick={() => navigate("/")}
+      >
+        RealtimeNote
+      </h1>
       {user && (
         <div className="mr-14">
           <div className="flex gap-5">
